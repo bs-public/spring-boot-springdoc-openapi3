dcbc9dab1906264dbd06926b4f7dd9a7e9cfd600
bs <26665808+bs-public@users.noreply.github.com>
2024-10-13 11:20:22 +1100

    Added springdoc documentation

--- src/main/java/com/springdoc/dto/UserDTO.java
+++ src/main/java/com/springdoc/dto/UserDTO.java
@@ -1,13 +1,20 @@
 package com.springdoc.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
 
 @Data
+@Schema(description = "Data Transfer Object for User information")
 public class UserDTO {
 
+  @Schema(
+      description = "The auto-generated unique id of the user",
+      accessMode = Schema.AccessMode.READ_ONLY)
   private Long id;
 
+  @Schema(description = "The name of the user")
   private String name;
 
+  @Schema(description = "The email of the user")
   private String email;
 }
